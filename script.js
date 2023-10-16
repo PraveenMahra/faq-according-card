34d4cd32040e959d698f27247cd53dda60abcb98
Praveen singh mahra <Praveen.mehra65@gmail.com>
2023-10-16 17:21:43 +0530

    add accordion only one at a time

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ faqItems.forEach((faq) => {
     const answer = this.parentElement.querySelector(".answer");
     const questionIcon = this.querySelector(".question-icon");
 
+    // Close all other FAQ items
+    faqItems.forEach((item) => {
+      if (item !== faq) {
+        const otherAnswer = item.querySelector(".answer");
+        const otherQuestion = item.querySelector(".question");
+        const otherQuestionIcon = otherQuestion.querySelector(".question-icon");
+
+        otherQuestion.classList.remove("question-active");
+        otherAnswer.classList.add("hidden");
+        otherQuestionIcon.classList.remove("icon-rotate");
+      }
+    });
+
+    // Toggle the clicked FAQ item
     question.classList.toggle("question-active");
     answer.classList.toggle("hidden");
     questionIcon.classList.toggle("icon-rotate");
